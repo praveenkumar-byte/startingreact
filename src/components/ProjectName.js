5a0a51f2e01a20f1033f21953917acce002cdfbd
praveenkumar-byte <praveenkumar6022@gmail.com>
2022-06-22 23:47:23 +0530

    commited changes

---
+++ src/components/ProjectName.js
@@ -0,0 +1,8 @@
+export default function ProjectNAme(){
+    return(
+        <div>
+        <h1 data-ns-test="project-name">Project Name</h1>
+        <p data-ns-test="project-description">this is project discription</p>
+        </div>
+    );
+}
